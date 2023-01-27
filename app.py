9a441247171addd8183c4c3fc468f58c5d369585
Axenos-dev <73383848+Axenos-dev@users.noreply.github.com>
2023-01-27 14:55:06 +0200

    styles and some features

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from generate_images import create_sample
 
 app = Flask("Landscape Diffusion")
 
-@app.route("/")
-def ren():
+@app.route("/", methods = ['GET'])
+def main_page():
     return render_template("main.html", img_path=".\static\generated_images\sample.jpg")
+
+
+@app.route("/generate", methods = ['GET', 'POST'])
+def generate_images():
+    create_sample(8)
+    
+    return redirect(url_for("main_page"))
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000)
